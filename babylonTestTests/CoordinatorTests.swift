33661a9ae803ec653d4e1e174936ba31949e837f
Amish Patel <amish@getsoda.com>
2019-06-07 18:07:52 +0100

    Added test for coordinator

---
+++ babylonTestTests/CoordinatorTests.swift
@@ -0,0 +1,66 @@
+//
+//  CoordinatorTests.swift
+//  babylonTestTests
+//
+//  Created by Amish Patel on 07/06/2019.
+//  Copyright © 2019 Amish Patel. All rights reserved.
+//
+
+import XCTest
+@testable import babylonTest
+
+class CoordinatorTests: XCTestCase {
+    
+    private var mainCoordinator: CoordinatorStub?
+    
+    override func setUp() {
+        mainCoordinator = CoordinatorStub()
+    }
+    
+    override func tearDown() {
+         mainCoordinator = nil
+    }
+    
+    func test_addChildCoordinator() {
+        
+        let coordinatorA = makeSUT()
+        
+        mainCoordinator?.addChildCoordinator(coordinator: coordinatorA)
+        
+        let coordinatorToTest = mainCoordinator?.childCoordinators[0]
+        
+        XCTAssert(coordinatorToTest === coordinatorA)
+    }
+    
+    func test_removeChildCoordinator() {
+        let coordinatorA = makeSUT()
+        
+        mainCoordinator?.addChildCoordinator(coordinator: coordinatorA)
+        
+        mainCoordinator?.removeChildCoordinator(coordinator: coordinatorA)
+        
+        XCTAssertEqual(0, mainCoordinator?.childCoordinators.count)
+    }
+    
+    func test_addChildCoordinator_onlyAddsOneInstance() {
+        let coordinatorA = makeSUT()
+        
+        mainCoordinator?.addChildCoordinator(coordinator: coordinatorA)
+        mainCoordinator?.addChildCoordinator(coordinator: coordinatorA)
+        
+        XCTAssertEqual(1, mainCoordinator?.childCoordinators.count)
+    }
+    
+    private func makeSUT() -> CoordinatorStub {
+        return CoordinatorStub()
+    }
+}
+
+
+private class CoordinatorStub: Coordinator {
+    var childCoordinators: [Coordinator] = [Coordinator]()
+    
+    func start() {
+        
+    }
+}
