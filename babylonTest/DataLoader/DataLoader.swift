cd5636446665563ce14ad89f058ce4f8329558b2
Amish Patel <amish@getsoda.com>
2019-06-07 23:20:04 +0100

    Implemented DataLoader class

--- babylonTest/DataLoader/DataLoader.swift
+++ babylonTest/DataLoader/DataLoader.swift
@@ -9,7 +9,48 @@
 import Foundation
 
 protocol DataLoadable {
-    func loadPosts(fromURL url: URL, completion: @escaping ([Post]) -> Void)
-    func loadUsers(fromURL url: URL, completion: @escaping ([User]) -> Void)
-    func loadComments(fromURL url: URL, completion: @escaping ([Comment]) -> Void)
+    func loadPosts(fromURL url: URL, completion: @escaping (Result<[Post], Error>) -> Void)
+    func loadUsers(fromURL url: URL, completion: @escaping (Result<[User], Error>) -> Void)
+    func loadComments(fromURL url: URL, completion: @escaping (Result<[Comment], Error>) -> Void)
+}
+
+final class DataLoader: DataLoadable {
+    
+    let httpClient: HTTPClient
+    let objectMapper: ObjectMapable
+    
+    init(httpClient: HTTPClient, objectMapper: ObjectMapable) {
+        self.httpClient = httpClient
+        self.objectMapper = objectMapper
+    }
+    
+    func loadPosts(fromURL url: URL, completion: @escaping (Result<[Post], Error>) -> Void) {
+        loadData(fromURL: url, completion: completion)
+    }
+    
+    func loadUsers(fromURL url: URL, completion: @escaping (Result<[User], Error>) -> Void) {
+        loadData(fromURL: url, completion: completion)
+    }
+    
+    func loadComments(fromURL url: URL, completion: @escaping (Result<[Comment], Error>) -> Void) {
+        loadData(fromURL: url, completion: completion)
+    }
+    
+    private func loadData<T: Codable>(fromURL url: URL, completion: @escaping (Result<[T], Error>) -> Void) {
+        httpClient.get(url: url) { (result) in
+            switch result {
+            case let .success(data):
+                do {
+                    let objets: [T] = try self.objectMapper.mapData(data: data)
+                    completion(.success(objets))
+                } catch let error {
+                    completion(.failure(error))
+                }
+                break
+            case let .failure(error):
+                completion(.failure(error))
+                break
+            }
+        }
+    }
 }
